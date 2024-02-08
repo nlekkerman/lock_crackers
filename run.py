11dc282b3732ec652b8d67b92b65c7387829ceab
nlekkerman <nlekkerman@gmail.com>
2024-02-08 11:02:00 +0000

    added inputrs for nmame and country with validation.

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+# Function to validate input
+def validate_name_country(input_string):
+    return all(char.isalpha() for char in input_string)
 
 
 # Google Sheets credentials and API setup
@@ -29,3 +32,22 @@ print("- Each '?' represents one number from 0 to 9.")
 print("- You have to guess the password by entering 6 numbers.")
 print("- If your guess matches the correct number, it will be revealed.")
 print()
+
+# Get player's name
+while True:
+    player_name = input("What's your name? ")
+    if validate_name_country(player_name):
+        break
+    else:
+        print("Please enter a valid name with only letters from 'a' to 'z'.")
+
+# Get player's country
+while True:
+    player_country = input("Where are you from? ")
+    if validate_name_country(player_country):
+        break
+    else:
+        print("Please enter a valid country with only letters from 'a' to 'z'.")
+
+# Display welcome message
+print(f"Welcome, {player_name} from {player_country}!")
