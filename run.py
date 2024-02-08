27fdbaffeda78d0e34bf109eb99b48135c8a5022
nlekkerman <nlekkerman@gmail.com>
2024-02-08 10:57:08 +0000

    Added creds for google sheet with scope

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+
+# Google Sheets credentials and API setup
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+
+# Open the specific spreadsheet
+SPREADSHEET = 'lock_crackers'
+SHEET = GSPREAD_CLIENT.open(SPREADSHEET)
+
+# Specify the worksheet to use
+WORKSHEET_NAME = 'info'
+worksheet = SHEET.worksheet(WORKSHEET_NAME)
