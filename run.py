a5bfdd7c3e7326fba8b842f85779534331786ecf
nlekkerman <nlekkerman@gmail.com>
2024-02-08 10:58:45 +0000

    added rules.

--- run.py
+++ run.py
@@ -20,3 +20,12 @@ SHEET = GSPREAD_CLIENT.open(SPREADSHEET)
 # Specify the worksheet to use
 WORKSHEET_NAME = 'info'
 worksheet = SHEET.worksheet(WORKSHEET_NAME)
+
+# Displaying rules
+print("Welcome to the Password Guessing Game!")
+print("Rules:")
+print("- You need to guess a password which consists of 6 numbers.")
+print("- Each '?' represents one number from 0 to 9.")
+print("- You have to guess the password by entering 6 numbers.")
+print("- If your guess matches the correct number, it will be revealed.")
+print()
