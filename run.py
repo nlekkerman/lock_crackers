545a470d5ecdae01ab20e1b1bb6c7ebdd805bd79
nlekkerman <nlekkerman@gmail.com>
2024-02-12 01:30:24 +0000

    Code adjusted to flake8 instructions.

--- run.py
+++ run.py
@@ -1,48 +1,79 @@
+"""
+Module Description: This module contains functions for"
+" interacting with Google Sheets using gspread.
+"""
+
 import gspread
 from google.oauth2.service_account import Credentials
 import random
 import time
 
-# ASCII Art
-ascii_art = """
-                                ...........................
-                                ...........................
-                                ..........===-===..........
-                                .........=.......=.........
-                                ........::.......::........
-                                ........:-:..:.::::........
-                                ........--+#####+--........
-                                .......:-###***###--.......
-                                ......-=+%%#%%%*%%+==......
-                                ......:+-%%%%@%%%%-=-......
-                                .......-=-%%@#@%%=+-.......
-                                .........+**++++**.........
-                                .............:.............
-                                ...........................
 
-"""
-
-# Display ASCII art
-print(ascii_art)
-# Function to validate input
 def validate_name_country(input_string):
+    """
+    Validates whether the input string contains only"
+    " alphabetic characters or spaces.
+
+    Parameters:
+    input_string (str): The string to be validated.
+
+    Returns:
+    bool: True if all characters in the input string are alphabetic or spaces,"
+    " False otherwise.
+    """
     return all(char.isalpha() or char.isspace() for char in input_string)
-# Function to validate game mode input
+
+
 def validate_game_mode(mode):
+    """
+    Validate the game mode.
+
+    Parameters:
+    mode (str): The game mode to validate.
+
+    Returns:
+    bool: True if the mode is valid ('C', 'E', or 'H'), False otherwise.
+    """
     return mode in ['C', 'E', 'H']
-# Function to validate password input based on difficulty level
+
+
 def validate_password(input_string, mode):
+    """
+    Validate the password input based on the difficulty level.
+
+    Parameters:
+    - input_string (str): The password input to be validated.
+    - mode (str): The difficulty level of the password validation "
+    "('C' for Easy, 'E' for Medium, 'H' for Hard).
+
+    Returns:
+    - bool: True if the password input is valid"
+    " according to the specified difficulty level, False otherwise.
+    """
     if mode == 'C':
-        return all(char.isdigit() and int(char) <= 3 for char in input_string.strip().split())
-    elif mode == 'E':
-        return all(char.isdigit() and int(char) <= 5 for char in input_string.strip().split())
-    elif mode == 'H':
-        return all(char.isdigit() and int(char) <= 9 for char in input_string.strip().split())
-    else:
-        return False
-# Function to validate confirmation input
+        return all(char.isdigit() and int(char) <= 3 
+                   for char in input_string.strip().split())
+    if mode == 'E':
+        return all(char.isdigit() and int(char) <= 5 
+                   for char in input_string.strip().split())
+    if mode == 'H':
+        return all(char.isdigit() and int(char) <= 9 
+                   for char in input_string.strip().split())
+    return False
+
+
 def validate_confirmation_input(input_string):
+    """
+    Validate whether the input string is either 'y' or 'n'.
+
+    Parameters:
+    input_string (str): The input string to be validated.
+
+    Returns:
+    bool: True if the input string is 'y' or 'n', False otherwise.
+    """
     return input_string.lower() in ['y', 'n']
+
 
 # Google Sheets credentials and API setup
 SCOPE = [
@@ -66,13 +97,15 @@ print("        ??? LOCK CRACKERS ???")
 print("Welcome to the Password Guessing Game!")
 print("Rules:")
 print("- You need to guess a password, which consists of 6 numbers.")
-print("- The numbers in the password are within a specific range depending on the difficulty level:")
+print("- The numbers in the password are within a specific range depending on "
+      "the difficulty level:")
 print("  - For 'C' (Child) mode, the numbers range from 0 to 3.")
 print("  - For 'E' (Easy) mode, the numbers range from 0 to 5.")
 print("  - For 'H' (Hard) mode, the numbers range from 0 to 9.")
 print("- Each '??' represents one number from the corresponding range.")
 print("- Your task is to guess the password by entering 6 numbers.")
-print("- If your guess contains the correct number at the correct position, it will be revealed.")
+print("- If your guess contains the correct number at the correct position,"
+      "it will be revealed.")
 print("- For example, if the password is '123456' and your guess is '143256',")
 print("  the revealed password will be '1*3*5*'.")
 print("- Use the revealed parts of the password to make subsequent guesses.")
@@ -84,16 +117,15 @@ while True:
     player_name = input("What's your name? ")
     if validate_name_country(player_name):
         break
-    else:
-        print("Please enter a valid name with only letters from 'a' to 'z'.")
+    print("Please enter a valid name with only letters from 'a' to 'z'.")
 
 # Get player's country
 while True:
     player_country = input("Where are you from? ")
     if validate_name_country(player_country):
         break
-    else:
-        print("Please enter a valid country with only letters from 'a' to 'z'.")
+    print("Please enter a valid country with only letters from 'a' to 'z'.")
+
 
 # Display welcome message
 print(f"Welcome, {player_name} from {player_country}!")
@@ -101,58 +133,67 @@ print(f"Welcome, {player_name} from {player_country}!")
 # Ask for game mode and start the timer
 start_time = time.time()
 while True:
-    mode = input("Choose the game mode - 'C' for child, 'E' for easy, or 'H' for hard: ").strip().upper()
-    if validate_game_mode(mode):
+    user_mode = input("Choose the game mode - 'C' for child, 'E' for easy,"
+                      " or 'H' for hard: ") \
+             .strip() \
+             .upper()
+
+    if validate_game_mode(user_mode):
         break
-    else:
-        print("Invalid input. Please enter 'C', 'E', or 'H'.")
+    print("Invalid input. Please enter 'C', 'E', or 'H'.")
+
 
 # Generate a random password based on game mode
-if mode == 'C':
+if user_mode == 'C':
     password = [random.randint(0, 3) for _ in range(6)]
-    difficulty_range = '0-3'
-elif mode == 'E':
+    DIFFICULTY_RANGE = '0-3'
+elif user_mode == 'E':
     password = [random.randint(0, 5) for _ in range(6)]
-    difficulty_range = '0-5'
-elif mode == 'H':
+    DIFFICULTY_RANGE = '0-5'
+elif user_mode == 'H':
     password = [random.randint(0, 9) for _ in range(6)]
-    difficulty_range = '0-9'
+    DIFFICULTY_RANGE = '0-9'
 
 hidden_password = ['?' for _ in range(6)]
 
 # Main game loop
 while True:
-    game_outcome = 'Ongoing'
+    GAME_OUTCOME = 'Ongoing'
     # Reset correct_numbers for each guess
-    correct_numbers = 0
+    CORRECT_NUMBERS = 0
     correct_positions = []  # List to store positions of correct numbers
 
     # Display hidden password
     print(" ".join(str(x) for x in hidden_password))
 
     # Get user's guess
-    guess = input(f"Enter your guess (6 numbers separated by spaces, or press 'q' to quit): ").strip()
+    guess = input("Enter your guess (6 numbers separated by spaces,"
+                  " or press 'q' to quit): ").strip()
 
+    # Check if user wants to quit
     # Check if user wants to quit
     if guess.lower() == 'q':
         while True:
-            confirm_quit = input("Are you sure you want to quit? (Y/N): ").strip().lower()
+            confirm_quit = input("Are you sure you want to quit? (Y/N): ") \
+                .strip() \
+                .lower()
             if validate_confirmation_input(confirm_quit):
                 if confirm_quit == 'y':
                     end_time = time.time()
                     elapsed_time = round(end_time - start_time, 1)
                     print(f"Elapsed time: {int(elapsed_time)} seconds")
-                    print("The password was:", " ".join(str(x) for x in password))
-                    game_outcome = 'Quit'
+                    print("The password was:", 
+                          " ".join(str(x) for x in password))
+                    GAME_OUTCOME = 'Quit'
                     break
-                elif confirm_quit == 'n':
+                if confirm_quit == 'n':
                     break
+
             else:
                 print("Invalid input. Please enter 'Y' or 'N'.")
-        if game_outcome == 'Quit':
+        if GAME_OUTCOME == 'Quit':
             break
-        else:
-            continue
+        continue
 
     # Check if the guess has 6 numbers
     guess_list = guess.split()
@@ -161,8 +202,9 @@ while True:
         continue
 
     # Validate the password input based on difficulty level
-    if not validate_password(guess, mode):
-        print(f"Please enter valid numbers within the difficulty range {difficulty_range}.")
+    if not validate_password(guess, user_mode):
+        print(f"Please enter valid numbers within the difficulty range "
+              f"{DIFFICULTY_RANGE}.")
         continue
 
     # Convert the guess to integers
@@ -172,20 +214,21 @@ while True:
     for i in range(6):
         if guess[i] == password[i]:
             hidden_password[i] = str(password[i])
-            correct_numbers += 1
-            correct_positions.append(i + 1)  # Append position of correct number
+            CORRECT_NUMBERS += 1
+            correct_positions.append(i + 1)
 
-    if correct_numbers == 6:
+    if CORRECT_NUMBERS == 6:
         print("Congratulations! You guessed the password correctly!")
-        game_outcome = 'Won'
+        GAME_OUTCOME = 'Won'
         break
     else:
         print("Incorrect guess. Try again.")
         if correct_positions:
-            print(f"Correct number/s found at position/s: {', '.join(map(str, correct_positions))}")
+            print(f"Correct number/s found at position/s: "
+                  f"{', '.join(map(str, correct_positions))}")
         else:
             print("Incorrect guess. Try again.")
-        game_outcome = 'Lost'
+        GAME_OUTCOME = 'Lost'
 
 # Stop stopwatch
 end_time = time.time()
@@ -193,8 +236,9 @@ elapsed_time = round(end_time - start_time, 1)
 print(f"Elapsed time: {int(elapsed_time)} seconds")
 
 # Saving player's information and game outcome to Google Sheets
-if game_outcome != 'Quit':  # Only append information to Google Sheets if the game wasn't quit
-    player_info = [player_name, player_country, mode, game_outcome, elapsed_time]
+if GAME_OUTCOME != 'Quit':
+    player_info = [player_name, player_country,
+                   user_mode, GAME_OUTCOME, elapsed_time]
     worksheet.append_row(player_info)
 
 # Printing the leaderboard based on the best times of players
@@ -203,4 +247,4 @@ print("Name        Country      Level     Status    Time")
 leaderboard_data = worksheet.get_all_values()[1:]
 leaderboard_data.sort(key=lambda x: float(x[-1]))
 for row in leaderboard_data:
-    print("{:<10} {:<14} {:<8} {:<8} {}".format(*row))
+    print(f"{row[0]:<10} {row[1]:<14} {row[2]:<8} {row[3]:<8} {row[4]}")
